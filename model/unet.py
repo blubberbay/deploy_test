d6a6abb79faf1d2e7d6b00b5693acb1f3c62161e
Jeff <jeffhawkins_email@yahoo.com>
2022-01-06 18:00:14 -0800

    adding model and cfg files

---
+++ model/unet.py
@@ -0,0 +1,66 @@
+from abs import ABC
+
+class BaseModel(ABC):
+	"""Abstract model class that is inherited to all models"""
+	
+	def __init__(self, cfg):
+		self.config = Config.from_json(cfg)
+	
+	@abstractmethod
+	def load_data(self):
+		pass
+	
+	@abstractmethod
+	def build(self):
+		pass
+	
+	@abstractmethod
+	def train(self):
+		pass
+		
+	@abstractmethod
+	def evaluate(self):
+		pass
+
+class UNet(BaseModel)
+	"""Unet Model class. Contains functionality for building, training and evaluating the model"""
+	
+	def __init__(self, config):
+		super().__init__(config)
+		self.base_model = tf.keras.applications.MobileNetV2(
+			input_shape=self.config.model.input, include_top = False)
+			
+	def load_data(self):
+		self.dataset, self.info = DataLoader().load_data(self.config.data )
+		self._preprocess_data()
+		
+	def build(self):
+	
+		self.model = tf.keras.Model(input = inputs, outputs = x )
+		
+	def train(self):
+		self.model.compile(
+			optimizer = self.config.train.optimizer.type
+			loss = tf.keras.losses.SparseCategoricalCrossentropy(
+				from_logits = True
+				),
+			metrics = self.config.train.metrics
+		)
+			
+		model_history = self.model.fit(
+			self.train_dataset,
+			epochs = self.epcohs
+			steps_per_epoch = self.steps_per_epoch
+			validation_steps = self.validation_steps
+			validation_data = self.validation_data
+		)
+		
+		return model_history.history['loss'], model_history.historl['val_loss']
+	
+	def evaluate(self):
+		predictions = []
+		for image, mask in self.dataset.take(1):
+			predictions.append(self.model.predict(image) )
+			
+		return predictions
+	
