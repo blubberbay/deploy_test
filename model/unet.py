b352c333404f191096ec12eeea1d5230066cb386
Jeff <jeffhawkins_email@yahoo.com>
2022-01-07 09:51:44 -0800

    adding _ functions

--- model/unet.py
+++ model/unet.py
@@ -22,7 +22,16 @@ class UNet(BaseModel)
 		self._preprocess_data()
 		
 	def build(self):
-	
+		
+		layer_names = [
+			"block_1_expand_relu", #64x64
+			"block_3_expand_relu", #32x32
+			"block_6_expand_relu", #16x16
+			"block_13_expand_relu", #8x8
+			"block_16_project", #4x4
+			]
+		layers = [self.base_model.get_layer(name).output for name in layer_names
+		
 		self.model = tf.keras.Model(input = inputs, outputs = x )
 		
 	def train(self):
@@ -51,3 +60,71 @@ class UNet(BaseModel)
 			
 		return predictions
 	
+	def _preprocess_data(self):
+		""" Splits into training and test and set training parameters"""
+		train = self.dataset['train'].map(
+			self.load_image_train,
+			num_parallel_calls = tf.data.experimental.AUTOTUNE
+		)
+		
+		self.train_dataset = train
+			.cache()
+			.shuffle(self.buffer_size)
+			.batch(self.batch_size)
+			.repeat()
+			
+		self.train_dataset = self.train_dataset.prefetch(
+			buffer_size = tf.data.experimental.AUTOTUNE
+		)
+		
+		self.test_dataset = test.batch(self.batch_size)
+		
+		
+		
+	def _set_training_parameters(self):
+		pass
+		
+	def _normalize(self, input_image, input_mask):
+		""" Normalise input image
+		
+		Args:
+			input_image (tf.image): The input image
+			input_mask (int): The image mask
+		
+		Returns:
+			input_image (tf.image): The normalized input image
+			input_mask (int): The new image mask
+		"""
+		
+		input_image = tf.cast(input_image, tf.float32) / 255.0
+		input_mask -= 1
+		return input_image, input_mask
+		
+	def _load_image_train(self, datapoint):
+		""" Loads and preprocess a single training image """
+		input_image = tf.image.resize(
+			datapoint['image'], (self.image_size, self.image_size )
+		)
+		
+		input_mask = tf.iage.resize(
+			datapoint['segmentation_mask'], (self.image_size, self.image_size )
+		)
+		
+		if tf.random.uniform(()) > 0.5:
+			input_image = tf.image.flip_left_right(input_image)
+			input_mask = tf.image.flip_left_right(input_mask)
+		
+		input_image, input_mask = self.normalize( input_image, input_mask )
+		
+		return input_image, input_mask
+		
+	def _load_image_test(self, datapoint):
+		""" Loads and preprocesss a single test image """
+		
+		input_image = tf.image.resize( datapoint['image'], (self.image_size, self.image_size ) )
+		
+		input_mask = tf.image.resize( datapoint['segmentation_mask'], (self.image_size, self.image_size ) )
+		
+		input_image, input_mask = self.normalize( input_image, input_mask )
+		
+		return input_image, input_mask
